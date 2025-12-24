5012ac99b73c2eeb051e5f04157330825bae5727
JasperCDev <jasperischauvin@gmail.com>
2025-12-23 23:56:44 -0600

    add-binary & merge-sorted-array

---
+++ src/add-binary.ts
@@ -0,0 +1,20 @@
+function addBinary(a: string, b: string): string {
+  let result = '';
+  let carry = 0;
+  let i = a.length - 1;
+  let j = b.length - 1;
+
+  while (i >= 0 || j >= 0 || carry > 0) {
+    const digitA = i >= 0 ? parseInt(a[i]) : 0;
+    const digitB = j >= 0 ? parseInt(b[j]) : 0;
+
+    const sum = digitA + digitB + carry;
+    result = (sum % 2) + result;
+    carry = Math.floor(sum / 2);
+
+    i--;
+    j--;
+  }
+
+  return result;
+}
